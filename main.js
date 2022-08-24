64fb3892ddc262785532a79cf8ea8726057e714c
Toshev Samandar <toshev1227@gmail.com>
2022-08-24 18:39:57 +0500

    example

---
+++ main.js
@@ -0,0 +1,85 @@
+
+// alert("Hello World");
+
+// console.log("Hello World");
+
+// o`zgaruvchilar - Variables
+// identifiers - O`zgaruvchi nomi
+
+// let ism = "Toshev Samandar"
+// let - keyword
+// ism - identifier
+// = - assignment operator
+// "Samandar" - value
+
+// console.log(ism);
+
+
+// primitiv data typs
+
+// Number
+// let x = 250;
+// let y = 40.5;
+
+
+
+// String
+// let str = 'Hello All';
+// let str1 = "Welcome to my new house";
+
+
+
+
+// undefined
+// let notGiven = undefined;
+
+
+// null
+// let x = null;
+
+
+// String
+// Number
+// Boolean
+
+// ==============================================================
+// 8ta ozgaruvchialr
+
+// Object
+
+// Function
+// Array
+// Object
+
+
+// ==============================================================
+
+
+// let const var 
+
+
+// operators
+let a = "24",
+    b = 24,
+    c = 9;
+
+// arithmetic operators
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % c); //model oparator
+// console.log(c** b);
+// console.log("oldin", a++);
+// console.log("Keyin", a);
+// console.log(++b); // preincrement oparator
+// console.log(--c); //predecrement operator
+
+
+// comparison operators
+// console.log(a > b); //true
+// console.log(b > a); //false
+// console.log(a <= b); //false
+// console.log(a == b); // a va b tengmi? false
+// console.log(a != b); // a va b tengmi? true
+console.log(a === b); // a va b tuei bir-xilmi yo qiymati tengmi? false
